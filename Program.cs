0db070373bd7a74d626e532018e2907086c14a27
AleksandraWoj <aleksandrawo99@gmail.com>
2019-11-09 20:13:27 +0200

    dodanie menu

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace filescan
     {
         static void Main(string[] args)
         {
+
+            MenuProgramu.StartMenuProgramu();
         }
     }
 }
--- menu.cs
+++ menu.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace filescan
 {
-    class menu
+    static class MenuProgramu
     {
+        public static void StartMenuProgramu()
+        {
+            Console.Title = " Menu Programu ";
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Liczba liter");
+                Console.WriteLine("2. Liczba wyrazów");
+                Console.WriteLine("3. Liczba znaków interpunkcyjnych ");
+                Console.WriteLine("4. Liczba zdań w pliku ");
+                Console.WriteLine("5. Raport użycia A-Z ");
+                Console.WriteLine("6. Koniec programu");
+
+                ConsoleKeyInfo klawisz = Console.ReadKey();
+                switch (klawisz.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.Clear(); opcjawbudowie(); break;
+                    case ConsoleKey.D2:
+                        Console.Clear(); opcjawbudowie(); break;
+                    case ConsoleKey.Escape:
+                    case ConsoleKey.D3:
+                        Console.Clear(); opcjawbudowie(); break;
+                    case ConsoleKey.D4:
+                        Console.Clear(); opcjawbudowie(); break;
+                    case ConsoleKey.D5:
+                        Console.Clear(); opcjawbudowie(); break;
+                    case ConsoleKey.D6:
+                        Environment.Exit(0); break;
+                    default: break;
+                }
+            }
+        }
+        static void opcjawbudowie()
+
+        {
+            Console.Write("Opcja w budowie ! ");
+            Console.ReadKey();
+   }
+
     }
 }
