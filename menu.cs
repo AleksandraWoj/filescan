33fa2342d24df71854af40acee5676aaeb1e4444
artur-kasprzak <meshu1994@hotmail.com>
2019-11-11 15:33:12 +0100

    pobieranie pliku z internetu

--- menu.cs
+++ menu.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace filescan
 {
@@ -13,10 +14,12 @@ namespace filescan
         {
             Console.Title = " Menu Programu ";
 
+            string url = "https://www.w3.org/TR/PNG/iso_8859-1.txt";
+
             while (true)
             {
                 Console.Clear();
-                Console.WriteLine("1. Liczba liter");
+                Console.WriteLine("1. Pobranie pliku z internetu");
                 Console.WriteLine("2. Liczba wyrazów");
                 Console.WriteLine("3. Liczba znaków interpunkcyjnych ");
                 Console.WriteLine("4. Liczba zdań w pliku ");
@@ -27,7 +30,7 @@ namespace filescan
                 switch (klawisz.Key)
                 {
                     case ConsoleKey.D1:
-                        Console.Clear(); opcjawbudowie(); break;
+                        Console.Clear(); pobraniePliku(url); break;
                     case ConsoleKey.D2:
                         Console.Clear(); opcjawbudowie(); break;
                     case ConsoleKey.Escape:
@@ -43,12 +46,23 @@ namespace filescan
                 }
             }
         }
-        static void opcjawbudowie()
-
-        {
+        static void opcjawbudowie(){
             Console.Write("Opcja w budowie ! ");
             Console.ReadKey();
-   }
+         }
+
+        public static void pobraniePliku(string url)
+        {
+            // plik pobiera sie do filescan\bin\Debug
+
+            string file = System.IO.Path.GetFileName(url);
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile(url, file);
+            Console.WriteLine("File has been downloaded...");
+            Console.ReadKey();
+        }
+
+
 
     }
 }
